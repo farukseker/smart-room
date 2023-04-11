e25dab16c795a7e0d02a90953d42aa8a089721a5
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-04-11 06:20:29 +0300

    set pg database

--- config/settings/product.py
+++ config/settings/product.py
@@ -6,11 +6,14 @@ from .base_settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': env('PGDATABASE'),
+        'USER': env('PGUSER'),
+        'PASSWORD': env('PGPASSWORD'),
+        'HOST': env('PGHOST'),
+        'PORT': env('PGPORT')
     }
 }
-
 
 CHANNEL_LAYERS = {
     "default": {
