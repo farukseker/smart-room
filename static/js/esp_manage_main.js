a2a3e1b5577135c48cec45d7e53811efce619161
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-07-19 17:09:04 +0300

    esp list view parsed

---
+++ static/js/esp_manage_main.js
@@ -0,0 +1,39 @@
+new Vue({
+    el:'#app',
+    data:{
+        connection: null,
+        esp_list: []
+    },
+    methods: {
+        send_key_change_status(key_id, status){
+            this.connection.send(JSON.stringify({
+                "type":"change_key_status_request",
+                "key_id":key_id,
+                "status":!status
+            }))
+        }
+    },
+
+    created: function() {
+    var vm = this; // Store reference to the Vue instance
+
+    console.log("Starting connection to WebSocket Server")
+
+    var origin = document.location.origin.replace("https","wss").replace("http","ws")
+
+    this.connection = new WebSocket(origin + "/ws/communication/user/")
+
+    this.connection.onmessage = function(event) {
+        console.log(event)
+
+        var data = JSON.parse(event.data)
+        console.log(data.esp_list)
+        vm.esp_list = data.esp_list
+        }
+
+    this.connection.onopen = function(event) {
+      console.log(event)
+      console.log("Successfully connected to the echo websocket server...")
+    }
+  }
+});
