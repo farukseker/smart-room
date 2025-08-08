13fb3e366602406780c1e6c375b3e5649e2d70b3
F4ruk.seker <ahmetfaruk731@gmail.com>
2025-08-08 20:28:48 +0300

    Change requirements file

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install -r requirements.txt
+pip install -r requirements.latest.txt
 gunicorn config.asgi:application DJANGO_SETTINGS_MODULE=config.settings.product -k uvicorn.workers.UvicornWorker --log-file -
